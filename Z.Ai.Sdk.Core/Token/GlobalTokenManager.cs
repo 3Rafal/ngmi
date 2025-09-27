cd96e766e8d74b1c0c80f4a91cf4702a8bb13381
Rafal Gwozdzinski <rafal@gwozdzinski.com>
2025-09-27 13:00:16 +0200

    Simplify GlobalTokenManager

--- Z.Ai.Sdk.Core/Token/GlobalTokenManager.cs
+++ Z.Ai.Sdk.Core/Token/GlobalTokenManager.cs
@@ -8,45 +8,17 @@ namespace Z.Ai.Sdk.Core.Token;
 /// </summary>
 public static class GlobalTokenManager
 {
-    private static volatile TokenManager? _globalTokenManager;
+    private static volatile TokenManager _globalTokenManager = new(LocalCache.GetInstance());
 
     /// <summary>
     /// Gets the global TokenManager instance for V4 API.
     /// </summary>
     /// <returns>TokenManager instance</returns>
-    public static TokenManager GetTokenManagerV4()
-    {
-        if (_globalTokenManager == null)
-        {
-            lock (typeof(GlobalTokenManager))
-            {
-                if (_globalTokenManager == null)
-                {
-                    _globalTokenManager = new TokenManager(LocalCache.GetInstance());
-                }
-            }
-        }
-        return _globalTokenManager;
-    }
+    public static TokenManager GetTokenManagerV4() => _globalTokenManager;
 
     /// <summary>
     /// Sets custom TokenManager implementation.
     /// </summary>
     /// <param name="tokenManager">Custom TokenManager instance</param>
-    public static void SetTokenManager(TokenManager tokenManager)
-    {
-        _globalTokenManager = tokenManager ?? throw new ArgumentNullException(nameof(tokenManager));
-    }
-
-    /// <summary>
-    /// Resets the global token manager to default implementation.
-    /// This is primarily used for testing purposes.
-    /// </summary>
-    public static void Reset()
-    {
-        lock (typeof(GlobalTokenManager))
-        {
-            _globalTokenManager = null;
-        }
-    }
+    public static void SetTokenManager(TokenManager tokenManager) => _globalTokenManager = tokenManager;
 }
